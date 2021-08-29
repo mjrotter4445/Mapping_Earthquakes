13f6a47c33a41caf00e78330333026b0aa3c9c68
Julie Rotter <mjrotter@msn.com>
2021-08-29 10:16:08 -0600

    Challenge uploads

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,79 @@
+
+///getting data from cities.js
+
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([34.0522, -118.2437], 2.25);
+
+
+
+// Get data from cities.js
+let cityData = cities;
+
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location).addTo(map);
+// });
+//Loop through the cities array and create one marker for each city.with bindpopup not formatted
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location)
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+//   .addTo(map);
+// });
+
+//new way Loop through the cities array and create one marker for each city with bindpopup and formatting
+//with dark background and yellow formatting 
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+    //bigger circle marker//radius: city.population/100000
+    radius: city.population/200000,
+    color: 'yellow',
+    // fillColor: '#CAC428',
+    fillColor: 'yellow',
+    fillOpacity: 0.2,
+    
+//     radius: 100
+})
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
+
+// old code  // Loop through the cities array and create one marker for each city.
+// cities.forEach(function(city) {
+//     console.log(city)
+// });
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location).addTo(map);
+// });
+
+// //var circle = L.circleMarker([34.0522, -118.2437], {
+//     color: 'black',
+//     fillColor: '#CAC428',
+//     fillOpacity: 0.4,
+//     radius: 100
+// }).addTo(map);
+
+
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+maxZoom: 18,
+accessToken: API_KEY
+});
+
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
